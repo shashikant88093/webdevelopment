003055124b779cff46c768bdfaaae34947f56b50
shashikant kumar <shashi94goswami@gmail.com>
2025-06-19 21:14:28 +0530

    completed the 64 box problem

--- React/box-color/src/App.jsx
+++ React/box-color/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const totalbox = 60;
+  const [boxColor, setBoxColor] = useState(0);
+
+  useEffect(() => {
+    let timeInterval = setInterval(() => {
+      setBoxColor((prev) => (prev + 1) % (totalbox + 1));
+    }, 1000);
+
+    return () => clearInterval(timeInterval);
+  }, [boxColor]);
+
+  const square = Array.from({ length: totalbox }, (_, i) => (
+    <div
+      className="square"
+      key={i}
+      style={{
+        backgroundColor: i < boxColor ? "white" : "black",
+      }}
+    />
+  ));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="grid">{square}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
