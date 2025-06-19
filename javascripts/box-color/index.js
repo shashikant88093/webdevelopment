18be6800feff957ccb50019639a134e558f1c6c2
shashikant kumar <shashi94goswami@gmail.com>
2025-06-19 21:43:35 +0530

    added the improvement

--- javascripts/box-color/index.js
+++ javascripts/box-color/index.js
@@ -1,11 +1,11 @@
 
-
+    const totalBox = 60
     let colorCount = 0
     function addWhiteColor() {
         let grid = document.querySelector(".grid");
         let timerTigger = setInterval(() => {
-            colorCount = (colorCount + 1) % 61;
-            let squares = Array.from({ length: 60 }, (_, i) => `<div class="square" id="${i}" style="background-color: ${i < colorCount ? "white" : "black"}"></div>`).join("");
+            colorCount = (colorCount + 1) % (totalBox + 1);
+            let squares = Array.from({ length: totalBox }, (_, i) => `<div class="square" id="${i}" style="background-color: ${i < colorCount ? "white" : "black"}"></div>`).join("");
             grid.innerHTML = squares;
         }, 1000);
 
